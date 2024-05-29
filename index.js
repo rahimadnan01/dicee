a15480af39b1aa4968f5202d4fc6db777f81d565
rahimadnan01 <hafizrahim3011@gmail.com>
2024-05-29 12:39:33 +0500

    Add files via upload
    I made a dicee game for you.

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6 +1);
+var randomNumber2 = Math.floor(Math.random() * 6 +1);
+var randomImage1 = "./images/dice"+randomNumber1+".png";
+var randomImage2 = "./images/dice"+randomNumber2+".png";
+document.querySelector(".img1").setAttribute("src",randomImage1);
+document.querySelector(".img2").setAttribute("src",randomImage2);
+function winner() {
+    if(randomNumber1===randomNumber2) {
+        document.querySelector("h1").innerHTML="draw"
+    }if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML="player 1 is winner"
+    } if(randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML="player 2 is winner"
+
+    }
+}
+winner();
